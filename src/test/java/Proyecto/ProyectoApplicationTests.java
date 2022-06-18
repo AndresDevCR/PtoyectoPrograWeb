bad1a8b0f21b17f005d5748baf1456447c94877b
Andrés Vargas <85952546+Skippy696@users.noreply.github.com>
2022-06-18 13:07:18 -0600

    first commit

---
+++ src/test/java/Proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
