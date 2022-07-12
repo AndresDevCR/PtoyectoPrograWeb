ded682c2583b7c0872b5e7819442288ab40f6acb
Andrés Vargas <85952546+Skippy696@users.noreply.github.com>
2022-07-11 18:23:22 -0600

    se agrego el modo administrador pero sin seguridad

--- src/main/java/Proyecto/Controllers/carComtroller.java
+++ src/main/java/Proyecto/Controllers/carComtroller.java
@@ -20,54 +20,14 @@ import java.nio.file.Files;
 
 @Controller
 public class carComtroller {
-    
+
     @Autowired
     private CarServiceImp carService;
-    
+
     @GetMapping("/")
     public String index(Model model) {
         var autos = carService.getAllCars();
         model.addAttribute("autos", autos);
         return "index";
     }
-
-    @GetMapping("/new")
-    public String newdata(Model model) {
-        return "";
-    }
-
-    @PostMapping("/add")
-    public String add(Car car,
-    @RequestParam("file") MultipartFile imagen) {
-        Path directorio = Paths.get("src//main//resources//static//img");
-        String ruta = directorio.toFile().getAbsolutePath();
-        try {
-            byte[] bytesImg = imagen.getBytes();
-            Path rutaArchivo = Paths.get(ruta + "//" + imagen.getOriginalFilename());
-            Files.write(rutaArchivo, bytesImg);
-            car.setImage(imagen.getOriginalFilename());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        carService.save(car);
-        return "redirect:/";
-    }
-   
-    
-
-  
-    @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Long id) {
-       Car car = carService.find(id);
-         carService.delete(car);
-        return "redirect:/";
-    }
-
-    @GetMapping("/edit/{id}")
-    public String edit(@PathVariable Long id, Model model) {
-        Car car = carService.find(id);
-        model.addAttribute("concierto", car);
-        return "edit";
-    }
-    
 }
