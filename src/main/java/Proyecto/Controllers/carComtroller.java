c6a6da13a6a9e7d8082ece79be3cb7a661385c2c
Andrés Vargas <85952546+Skippy696@users.noreply.github.com>
2022-07-13 14:06:06 -0600

    cambio a un controlador

--- src/main/java/Proyecto/Controllers/carComtroller.java
+++ src/main/java/Proyecto/Controllers/carComtroller.java
@@ -1,22 +1,10 @@
 package Proyecto.Controllers;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
-import Proyecto.Models.Car;
 import Proyecto.Services.CarServiceImp;
-
-import java.nio.file.Files;
 
 @Controller
 public class carComtroller {
